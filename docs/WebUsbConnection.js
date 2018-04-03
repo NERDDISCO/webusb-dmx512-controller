5a336bf8ac2149642e06e638eb732a6fa03fe0fb
Tim Pietrusky <timpietrusky@gmail.com>
2018-04-04 00:35:48 +0200

    fix(everything):  :D

--- docs/WebUsbConnection.js
+++ docs/WebUsbConnection.js
@@ -102,7 +102,7 @@ export default class WebUsbConnection {
   }
 
   log(message, data) {
-    let fullMessage = `${message}` + productName
+    let fullMessage = `${message}`
 
     if (data instanceof USBDevice) {
       const { productName, manufacturerName, configuration } = data
